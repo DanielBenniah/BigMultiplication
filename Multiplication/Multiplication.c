b18c12ab5336688b6addde07402c3aebef13a57a
Daniel Benniah <danielbenniah@berkeley.edu>
2018-09-14 00:53:56 -0700

    Add files via upload

---
+++ Multiplication/Multiplication.c
@@ -0,0 +1,38 @@
+#include<stdio.h> 
+  
+// Returns (a * b) % mod 
+long long moduloMultiplication(long long a, 
+                               long long b, 
+                               long long mod) 
+{ 
+    long long res = 0;  // Initialize result 
+  
+    // Update a if it is more than 
+    // or equal to mod 
+    a %= mod; 
+  
+    while (b) 
+    { 
+        // If b is odd, add a with result 
+        if (b & 1) 
+            res = (res + a) % mod; 
+  
+        // Here we assume that doing 2*a 
+        // doesn't cause overflow 
+        a = (2 * a) % mod; 
+  
+        b >>= 1;  // b = b / 2 
+    } 
+  
+    return res; 
+} 
+  
+// Driver program 
+int main() 
+{ 
+    long long a = 10123465234878998; 
+    long long b = 65746311545646431; 
+    long long m = 10005412336548794; 
+    printf("%lld", moduloMultiplication(a, b, m)); 
+    return 0; 
+} 
---
+++ Multiplication/Multiplication.py
@@ -0,0 +1,5 @@
+def multiply (a, b):
+	return a * b
+
+if __name__ == "__main__":
+	print (multiply(99999999, 9999999))
